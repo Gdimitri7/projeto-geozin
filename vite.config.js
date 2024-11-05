01b749d619a3631da78a47b81e7b098243d7957d
Gustavo Silva <a46048@alunos.ipb.pt>
2024-11-05 16:10:23 +0000

    .

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import reactRefresh from '@vitejs/plugin-react';
+import svgrPlugin from 'vite-plugin-svgr';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+
+ build: {
+     outDir: 'build',
+ },
+ plugins: [
+     reactRefresh(),
+     svgrPlugin({
+         svgrOptions: {
+             icon: true,
+         },
+     }),
+ ],
+
 })
